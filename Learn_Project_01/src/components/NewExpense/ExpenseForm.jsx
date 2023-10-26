9413a0550d15444288384767aa55f91f8be6db0e
BaeDongWoo <ehddn5410@naver.com>
2023-10-26 21:27:19 +0900

    title,date,amount 저장 및 제출

--- Learn_Project_01/src/components/NewExpense/ExpenseForm.jsx
+++ Learn_Project_01/src/components/NewExpense/ExpenseForm.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+  const amoutHandler = (e) => {
+    setAmount(e.target.value);
+  };
+  const dateHandler = (e) => {
+    setDate(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+    console.log(expenseData);
+    setTitle('');
+    setAmount('');
+    setDate('');
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} value={title} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amoutHandler}
+            value={amount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateHandler}
+            value={date}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
